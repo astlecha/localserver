b091cf960d94396df0fa8ceace97b9805e17798c
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-07-19 11:03:21 -0700

    adds pseudocode comments

--- prac_server.js
+++ prac_server.js
@@ -1,26 +1,30 @@
 var http = require("http");
 
+//Define the ports
 var PORT1 = 7000;
 var PORT2 = 7500;
 
+//Take server requests and responses
 function handleRequest(request, response){
 	response.write("You are amazin!!!");
 	response.end("Path hit: "+request.url);
 }
-
-var server = http.createServer(handleRequest);
-
-server.listen(PORT1, function(){
-	console.log("Server listening on: http://localhost:%s", PORT1);
-})
 
 function handleRequest_2(request, response){
 	response.write("Your breath STANKS");
 	response.end("Path hit: "+request.url);
 }
 
-var server = http.createServer(handleRequest_2);
+//===========================Create Servers=======================================
+var server = http.createServer(handleRequest);
+var server_2 = http.createServer(handleRequest_2);
+//==================================================================
 
-server.listen(PORT2, function(){
+//Listen for requests on each port
+server.listen(PORT1, function(){
+	console.log("Server listening on: http://localhost:%s", PORT1);
+})
+
+server_2.listen(PORT2, function(){
 	console.log("Server listening on: http://localhost:%s", PORT2);
 })
