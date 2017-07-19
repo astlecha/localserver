88e9111989e239fd67a2fc229356bd64256dad52
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-07-19 10:45:41 -0700

    logs different things on different ports

--- prac_server.js
+++ prac_server.js
@@ -1,8 +1,20 @@
-var http = require('http');
+var http = require("http");
 
 var PORT1 = 7000;
 var PORT2 = 7500;
 
 function handleRequest(request, response){
-	response.end('Path hit: '+request.url);
+	response.end("Path hit: "+request.url);
 }
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT1, function(){
+	console.log("Server listening on: http://localhost:%s", PORT1);
+	console.log("You are amazin!!!");
+})
+
+server.listen(PORT2, function(){
+	console.log("Server listening on: http://localhost:%s", PORT2);
+	console.log("Your breath STANKS");
+})
